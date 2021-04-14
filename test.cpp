2e4cca3e92f642659e37a3d95002840348ecfae6
StrategFirst <mathieu.toulon@free.fr>
2021-04-15 01:34:51 +0200

    Improve test file

--- test.cpp
+++ test.cpp
@@ -1,6 +1,26 @@
 #include "consolecolor.hh"
+#include <iostream>
 
 int main() {
-	std::cout << console::color(30,80,200) << "TEST" << std::endl << console::reset ;
+	std::cout << "\u0010 " << "Color testing : " << std::endl;
+	std::cout << console::color(255,0,0) << "This must be red!" << console::reset << std::endl;
+	std::cout << console::color(0,255,0) << "This must be green!" << console::reset << std::endl;
+	std::cout << console::color(0,0,255) << "This must be blue!" << console::reset << std::endl;
+	std::cout << console::color(0,255,255) << "This must be cyan!" << console::reset << std::endl;
+	std::cout << console::color(255,0,255) << "This must be purple!" << console::reset << std::endl;
+	std::cout << console::color(255,255,0) << "This must be yellow!" << console::reset << std::endl;
+	std::cout << console::color(255,255,255) << "This must be white!" << console::reset << std::endl;
+	std::cout << console::color(75,75,75) << "This must be a dark gray!" << console::reset << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "\u0010 " << "Security testing : " << std::endl;
+	try {
+		std::cout << console::color(42,666,69) << " /!\\ " << "This text shouldn't be displayed ! " << console::reset << std::endl;
+	} catch( std::range_error e ) {
+		std::cout << console::color(42,255,69) << " [V] " << "Test passed " << console::reset << std::endl;
+	}
+	std::cout << std::endl;
+
+
 	return EXIT_SUCCESS;
 }
